90e00e325ef12f92127dba651c7edc17de22406b
JaeKeoung <tkdlgks@naver.com>
2017-03-06 10:45:09 +0900

    웹 크롤링 해오기

---
+++ scraping.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import urllib.request
+response=urllib.request.urlopen('http://www.clien.net/cs2/bbs/board.php?bo_table=news')
+page=response.read().decode('utf-8', 'ignore')
+#print (page)
+soup=BeautifulSoup(page, 'html5lib')
+#print (soup)
+list=soup.findAll('td',attrs={'class':'post_subject'})
+for item in list:
+    title=item.find('a').text
+    print(title)
